546a2376abf5b8202d3fd68873019e1d61ca49a1
Mathieu <mathieu.ancelin@gmail.com>
2016-01-25 15:49:47 +0100

    Add readme

--- readme.md
+++ readme.md
@@ -16,7 +16,61 @@ or
 
 ## Validate your state
 
-TODO
+Sometimes it can be helpful to validate your component state when in dev mode, even if you should be sure of what kind of data are merged to the state.
+
+With `react-state-validator` you can reuse the React validation API for props to validate your state. You just have to defined a `stateTypes` property on your component, and pass the component into an enhancer. You can use the `enhanceWithValidState` API to create an Higher Order Component that can work with `React.Component` or `React.createClass` as the following code.
+
+**WARNING**
+
+As the validation is only performed in dev mode (for performance reason), in dev mode you should define a `process.env.NODE_ENV` variable with a value that is not `production` (ie. `test`, or `dev`). In production mode, set the same variable to `production`.
+
+```javascript
+import React from 'react';
+import { enhanceWithValidState } from 'react-state-validator';
+
+export const HellWorld = enhanceWithValidState(React.createClass({
+  getInitialState() {
+    return {
+      name: 'World',
+    };
+  },
+  stateTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
+  render() {
+    return (
+      <h1>Hello {this.state.name}!</h1>
+    );
+  }
+}));
+```
+
+or
+
+```javascript
+import React from 'react';
+import { enhanceWithValidState } from 'react-state-validator';
+
+class HellWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+  }
+  render() {
+    return (
+      <h1>Hello {this.state.name}!</h1>
+    );
+  }
+}
+
+HellWorld.stateTypes = {
+  name: React.PropTypes.string.isRequired,
+};
+
+export default enhanceWithValidState(HellWorld);
+```
+
+Each time you state doesn't comply to validation constraints in dev mode, a error will be logged in your console.
 
 [1]: https://api.travis-ci.org/mathieuancelin/react-state-validator.svg
 [2]: https://api.travis-ci.org/mathieuancelin/react-state-validator
--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import { validate } from './statevalidator';
 
 const listeners = [];
 
+function getGlobalObject() {
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+  if (typeof window !== 'undefined') {
+    return window;
+  }
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+  return new Function('return this')(); // eslint-disable-line
+}
+
+const globalObject = getGlobalObject();
+if (!globalObject.process) globalObject.process = {};
+if (!globalObject.process.env) globalObject.process.env = {};
+if (!globalObject.process.env.hasOwnProperty('NODE_ENV')) globalObject.process.env.NODE_ENV = 'production';
+
+function broadcast(name, errors) {
+  listeners.forEach(l => l(name, errors));
+}
+
 export function __subscribeToErrors(listener) {
   listeners.push(listener);
   return () => {
@@ -12,21 +34,18 @@ export function __subscribeToErrors(listener) {
   };
 }
 
-function broadcast(name, errors) {
-  listeners.forEach(l => l(name, errors));
-}
-
-export function enhanceWithValidState(Component) {
+export function enhanceWithValidState(Component, run) {
+  const shouldRun = run || process.env.NODE_ENV !== 'production';
   return class Enhancer extends Component {
     constructor(props) {
       super(props);
       const name = Component.name || Component.displayName;
       const stateTypes = this.constructor.stateTypes || this.stateTypes;
-      if (process.env.NODE_ENV !== 'production' && stateTypes) {
+      if (shouldRun && stateTypes) {
         const errors = validate(this.state, stateTypes, name);
         if (errors.length > 0) {
           broadcast(name, errors);
-          errors.forEach(e => console.warn(`Warning: Failed stateType: ${e.message}`));
+          errors.forEach(e => console.error(`Warning: Failed stateType: ${e.message}`));
         }
       }
     }
@@ -34,11 +53,11 @@ export function enhanceWithValidState(Component) {
       if (super.componentWillUpdate) super.componentWillUpdate(nextProps, nextState);
       const name = Component.name || Component.displayName;
       const stateTypes = this.constructor.stateTypes || this.stateTypes;
-      if (process.env.NODE_ENV !== 'production' && stateTypes) {
+      if (shouldRun && stateTypes) {
         const errors = validate(nextState, stateTypes, name);
         if (errors.length > 0) {
           broadcast(name, errors);
-          errors.forEach(e => console.warn(`Warning: Failed stateType: ${e.message}`));
+          errors.forEach(e => console.error(`Warning: Failed stateType: ${e.message}`));
         }
       }
     }
